f3c0390e33342956c931a10a0455be0fb711e85c
Jhonny Bravo <sanfranceshika5@gmail.com>
2016-07-22 02:21:28 +0900

    modified: phantomjs/sh/provision_package.sh
    shellcheck によるコーディングスタイルの修正。

--- phantomjs/sh/provision_package.sh
+++ phantomjs/sh/provision_package.sh
@@ -36,10 +36,9 @@ exit 1
 }
 
 function install_package(){
-  package_dir=$(ls ../ | grep "$1" | sed -e "s/.tar.bz2//")
-
   (
     cd ../
+    package_dir=$(find . -name "phantomjs*${1}*" | sed -e "s/.tar.bz2//")
     tar jxf "${package_dir}.tar.bz2"
     source_path="${package_dir}/bin/${package_name}"
     install "$source_path" "$destination_path"
@@ -74,9 +73,9 @@ if [ $i_flag -eq 1 ]; then
   shift $((OPTIND - 2))
   architecture="$1"
 
-  if [ $architecture = "32bit" ]; then
+  if [ "$architecture" = "32bit" ]; then
     install_package "i686"
-  elif [ $architecture = "64bit" ]; then
+  elif [ "$architecture" = "64bit" ]; then
     install_package "x86_64"
   fi
 elif [ $u_flag -eq 1 ]; then
