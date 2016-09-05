7e8971a3d947c4110336050dc838ce9b7e6c4f3a
Jhonny Bravo <sanfranceshika5@gmail.com>
2016-09-05 21:19:51 +0000

    modified: powershell/sh/get_package.sh
    deleted function: get_package
    deleted option: v

--- powershell/sh/get_package.sh
+++ powershell/sh/get_package.sh
@@ -2,19 +2,18 @@
 
 script_name=$(basename "$0")
 package_name="powershell"
-v_flag=0
 
 function usage(){
 cat <<_EOT_
 NAME
-  ${script_name} [-v version]
+  ${script_name} version
   ${script_name} -h
 
 DESCRIPTION
   ${package_name} をダウンロードします。
 
 OPTIONS
-  -v version
+  version
     OS のバージョンに合わせたパッケージをダウンロードします。
 
     有効な値:
@@ -28,20 +27,9 @@ _EOT_
 exit 1
 }
 
-function get_package(){
-  (
-    cd ../
-    url=$(grep "$1" -a ubuntu/url_list.txt | awk '{print $2}')
-    wget "$url"
-  )
-}
-
-while getopts "v:h" option
+while getopts "h" option
 do
   case $option in
-    v)
-      v_flag=1
-      ;;
     h)
       usage
       ;;
@@ -51,13 +39,12 @@ do
   esac
 done
 
-if [ $v_flag -eq 1 ]; then
-  shift $((OPTIND - 2))
-  version="$1"
+version="$1"
 
-  if [ "$version" = "14.04" ]; then
-    get_package "14.04"
-  elif [ "$version" = "16.04" ]; then
-    get_package "16.04"
-  fi
+if [ "$version" = "14.04" ]; then
+  url=$(awk '/14.04/ {print $2}' < ../ubuntu/url_list.txt)
+  wget "$url"
+elif [ "$version" = "16.04" ]; then
+  url=$(awk '/16.04/ {print $2}' < ../ubuntu/url_list.txt)
+  wget "$url"
 fi
