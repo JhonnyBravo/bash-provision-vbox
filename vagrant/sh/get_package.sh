570e502dfb64c5038cfcf6838a69a0afc58a4055
Jhonny Bravo <sanfranceshika5@gmail.com>
2016-09-06 10:43:39 +0000

    modified: vagrant/sh/provision_package.sh
    deleted function: get_package
    deleted option: a

--- vagrant/sh/get_package.sh
+++ vagrant/sh/get_package.sh
@@ -2,19 +2,18 @@
 
 script_name=$(basename "$0")
 package_name="vagrant"
-a_flag=0
 
 function usage(){
 cat <<_EOT_
 NAME
-  ${script_name} [-a architecture]
+  ${script_name} architecture
   ${script_name} -h
 
 DESCRIPTION
   ${package_name} をダウンロードします。
 
 OPTIONS
-  -a architecture
+  architecture
     OS のアーキテクチャに合わせたパッケージをダウンロードします。
 
     有効な値:
@@ -28,20 +27,9 @@ _EOT_
 exit 1
 }
 
-function get_package(){
-  (
-    cd ../
-    url=$(grep "$1" -a debian/url_list.txt | awk '{print $2}')
-    wget "$url"
-  )
-}
-
-while getopts "a:h" option
+while getopts "h" option
 do
   case $option in
-    a)
-      a_flag=1
-      ;;
     h)
       usage
       ;;
@@ -51,13 +39,12 @@ do
   esac
 done
 
-if [ $a_flag -eq 1 ]; then
-  shift $((OPTIND - 2))
-  architecture="$1"
+architecture="$1"
 
-  if [ "$architecture" = "32bit" ]; then
-    get_package "i686"
-  elif [ "$architecture" = "64bit" ]; then
-    get_package "x86_64"
-  fi
+if [ "$architecture" = "32bit" ]; then
+  url=$(awk '/i686/ {print $2}' < ../debian/url_list.txt)
+  wget "$url"
+elif [ "$architecture" = "64bit" ]; then
+  url=$(awk '/x86_64/ {print $2}' < ../debian/url_list.txt)
+  wget "$url"
 fi
